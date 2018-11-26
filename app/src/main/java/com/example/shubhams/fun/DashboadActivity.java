07e9353317b1c99655ef072db8cb4f9a1db1b39d
sudhir.rawat <sudhir.rawat@inubesolutions.com>
2018-11-26 16:24:03 +0530

    dasboard UI

---
+++ app/src/main/java/com/example/shubhams/fun/DashboadActivity.java
@@ -0,0 +1,14 @@
+package com.golmal.fun;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class DashboadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboad);
+    }
+}
